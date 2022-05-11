e53a48b11e255220cb5cc5f0ef860559e91e631b
yoga <Gem_yoga@163.com>
2022-05-11 10:30:10 +0800

    :pencil2: add common beans

---
+++ universe-common/src/main/java/org/yoga/universe/utils/TransactionUtils.java
@@ -0,0 +1,15 @@
+package org.yoga.universe.utils;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+/**
+ * @Description: 事务工具类
+ * @Author: yoga
+ * @Date: 2022/5/11 10:00
+ */
+public class TransactionUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionUtils.class);
+
+}
