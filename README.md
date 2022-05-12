75da72c8805527595e478972cf79325dc1d0e0ec
yoga <Gem_yoga@163.com>
2022-05-12 14:32:57 +0800

    :wrench: update pom, add the copy of the Apache License, update README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # universe
+
+### 本地运行
+在universe目录下执行下面脚本，在本地仓库install universe-dependencies-bom的模块，然后再导入整个项目的依赖
+
+``` shell
+mvn clean install -Dmaven.test.skip=true -pl universe-dependencies-bom -am
+```
+
+提示：等后续上传到maven仓库就可以不需要这一步了
--- universe-common/src/main/java/org/yoga/universe/utils/RSAUtils.java
+++ universe-common/src/main/java/org/yoga/universe/utils/RSAUtils.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2022 yoga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.yoga.universe.utils;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
-
-import java.security.*;
-import java.util.Base64;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 /**
  * @Description: RSA加密工具类
@@ -13,14 +29,10 @@ import java.util.Base64;
  */
 public class RSAUtils {
 
-    private static final Logger logger = LoggerFactory.getLogger(RSAUtils.class);
-
     public static void main(String[] args) throws NoSuchAlgorithmException {
         KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
         KeyPair keyPair = generator.genKeyPair();
         PublicKey publicKey = keyPair.getPublic();
         PrivateKey privateKey = keyPair.getPrivate();
-        logger.debug(String.format("test info %s : %s", Base64.getEncoder().encodeToString(publicKey.getEncoded()),
-                Base64.getEncoder().encodeToString(privateKey.getEncoded())));
     }
 }
