df4c51704771b2cc04214e7d930f15530109d7c6
yoga <Gem_yoga@163.com>
2022-05-14 22:36:05 +0800

    add more oss operate

--- universe-oss/src/main/java/org/yoga/universe/OssService.java
+++ universe-oss/src/main/java/org/yoga/universe/OssService.java
@@ -18,23 +18,21 @@ package org.yoga.universe;
 
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.common.auth.DefaultCredentialProvider;
-import com.aliyun.oss.model.GetObjectRequest;
-import com.aliyun.oss.model.OSSObject;
-import com.aliyun.oss.model.ObjectMetadata;
-import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.*;
 import org.springframework.lang.Nullable;
 import org.yoga.universe.beans.OssResourceDTO;
 import org.yoga.universe.constants.DelimiterType;
+import org.yoga.universe.exception.UniverseException;
 import org.yoga.universe.listener.OssProgressListener;
+import org.yoga.universe.utils.IOUtils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Map;
-import java.util.StringJoiner;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @Description: TODO
@@ -69,12 +67,12 @@ public class OssService {
     private String bucketName;
 
     /**
-     * 授权密钥Id
+     * access key of oss
      */
     private String accessKeyId;
 
     /**
-     * 授权密钥Secret
+     * secret key of oss
      */
     private String secretAccessKey;
 
@@ -98,7 +96,7 @@ public class OssService {
     /**
      * upload resource to oss
      *
-     * @param inputStream   文件流
+     * @param inputStream   resource
      * @param resourceName  name of resource
      * @param resourceSize  size of resource
      * @param folder        oss folder, Use the default folder {@value DEFAULT_FOLDER} when the folder is blank
@@ -136,10 +134,90 @@ public class OssService {
     }
 
     /**
+     * multipart upload resource to oss
+     * for larger resource
+     *
+     * @param bytes        byte array of resource
+     * @param resourceSize name of resource
+     * @param folder       oss folder, Use the default folder {@value DEFAULT_FOLDER} when the folder is blank
+     * @param objectName   objectName
+     * @param partSize     partSize(min value 100mb)
+     * @throws UniverseException exception
+     */
+    private OssResourceDTO multipartUpload(byte[] bytes, long resourceSize, @Nullable String folder, @Nullable String objectName,
+                                           long partSize) throws UniverseException {
+//        Assert.notNull(bytes, "bytes is null");
+//        Assert.isTrue(MIN_PART_SIZE < partSize, String.format("partSize[%d] smaller than min partSize", partSize));
+
+        OssResourceDTO ossResourceDTO = new OssResourceDTO();
+        ossResourceDTO.setResourceSize(resourceSize);
+
+        objectName = handleObjectName(objectName, folder);
+        OSSClient ossClient = generateOssClient();
+        InitiateMultipartUploadResult initiateMultipartUploadResult =
+                ossClient.initiateMultipartUpload(new InitiateMultipartUploadRequest(bucketName, objectName));
+        String uploadId = initiateMultipartUploadResult.getUploadId();
+        List<PartETag> tags = new ArrayList<>();
+        int partCount = (int) Math.ceil((double) resourceSize / partSize);
+
+        for (int i = 0; i < partCount; i++) {
+            long startPos = i * partSize;
+            try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes)) {
+                byteArrayInputStream.skip(startPos);
+                UploadPartRequest uploadPartRequest = new UploadPartRequest();
+                uploadPartRequest.setBucketName(bucketName);
+                uploadPartRequest.setKey(objectName);
+                uploadPartRequest.setUploadId(uploadId);
+                uploadPartRequest.setInputStream(byteArrayInputStream);
+                uploadPartRequest.setPartSize((i + 1 == partCount) ? (resourceSize - startPos) : partSize);
+                uploadPartRequest.setPartNumber(i + 1);
+                // upload
+                UploadPartResult uploadPartResult =
+                        ossClient.uploadPart(uploadPartRequest.withProgressListener(new OssProgressListener()));
+                tags.add(uploadPartResult.getPartETag());
+            } catch (IOException e) {
+                throw new UniverseException("multipart upload to oss fail", e);
+            }
+        }
+
+        tags.sort(Comparator.comparingInt(PartETag::getPartNumber));
+
+        CompleteMultipartUploadRequest completeMultipartUploadRequest =
+                new CompleteMultipartUploadRequest(bucketName, objectName, uploadId, tags);
+        // complete
+        ossClient.completeMultipartUpload(completeMultipartUploadRequest.withProgressListener(new OssProgressListener()));
+        shutdownOssClient(ossClient);
+        ossResourceDTO.setObjectName(objectName);
+        return ossResourceDTO;
+    }
+
+    /**
+     * download file from oss
+     *
+     * @param objectName  objectName
+     * @param securityKey securityKey of resource
+     * @param isEncrypt   Whether to encrypt
+     * @return byte array of resource
+     */
+    public byte[] download(String objectName, @Nullable String securityKey, boolean isEncrypt) throws IOException {
+        if (isEncrypt && (null == securityKey || securityKey.length() == 0)) {
+            throw new UniverseException("securityKey is illegal");
+        }
+        OSSClient ossClient = generateOssClient();
+        OSSObject ossObject = ossClient.getObject(new GetObjectRequest(bucketName, objectName)
+                .withProgressListener(new OssProgressListener()));
+        try (InputStream inputStream = ossObject.getObjectContent()) {
+            return IOUtils.toByteArray(inputStream);
+        } finally {
+            shutdownOssClient(ossClient);
+        }
+    }
+
+    /**
      * download resource from oss by url
      *
      * @param ossUrl         oss resource url
-     * @param requestHeaders requestH headers
+     * @param requestHeaders request headers
      * @return byte array of resource
      */
     public byte[] downloadByOssUrl(String ossUrl, @Nullable Map<String, String> requestHeaders) throws IOException {
@@ -148,11 +226,37 @@ public class OssService {
         OSSObject ossObject = ossClient.getObject(new GetObjectRequest(url, requestHeaders)
                 .withProgressListener(new OssProgressListener()));
         try (InputStream inputStream = ossObject.getObjectContent()) {
-            // IOUtils.toByteArray(inputStream); TODO
-            return null;
+            return IOUtils.toByteArray(inputStream);
         } finally {
             shutdownOssClient(ossClient);
         }
+    }
+
+    /**
+     * generate oss temporary url
+     *
+     * @param objectName objectName
+     * @param expireTime url expire time
+     * @return url
+     */
+    public URL generateOssUrl(String objectName, long expireTime) {
+        OSSClient ossClient = generateOssClient();
+        Date expiration = new Date(System.currentTimeMillis() + expireTime);
+        URL url = ossClient.generatePresignedUrl(bucketName, objectName, expiration);
+        shutdownOssClient(ossClient);
+        return url;
+    }
+
+    /**
+     * delete from oss
+     *
+     * @param objectName objectName
+     */
+    public void delete(String objectName) {
+        OSSClient ossClient = generateOssClient();
+        ossClient.deleteObject(new GenericRequest(bucketName, objectName)
+                .withProgressListener(new OssProgressListener()));
+        shutdownOssClient(ossClient);
     }
 
     /**
