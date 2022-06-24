f6c8f29ab4a3b45bfbe17f97ffa1437fe4d09044
yoga <Gem_yoga@163.com>
2022-06-24 21:39:07 +0800

    add start module

---
+++ jarvis-samples/jarvis-samples-ddd-shop/jarvis-samples-ddd-shop-start/src/main/java/org/yoga/jarvis/jarvissamplesdddshopstart/JarvisSamplesDddShopStartApplication.java
@@ -0,0 +1,13 @@
+package org.yoga.jarvis.jarvissamplesdddshopstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarvisSamplesDddShopStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarvisSamplesDddShopStartApplication.class, args);
+    }
+
+}
---
+++ jarvis-samples/jarvis-samples-ddd-shop/jarvis-samples-ddd-shop-start/src/test/java/org/yoga/jarvis/jarvissamplesdddshopstart/JarvisSamplesDddShopStartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yoga.jarvis.jarvissamplesdddshopstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarvisSamplesDddShopStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
