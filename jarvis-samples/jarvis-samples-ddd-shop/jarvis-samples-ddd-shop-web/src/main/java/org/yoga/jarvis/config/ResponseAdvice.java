6c583641fa05467b082ec5363dbfd30a219e2077
yoga <Gem_yoga@163.com>
2022-08-16 18:06:29 +0800

    add ResponseAdvice

---
+++ jarvis-samples/jarvis-samples-ddd-shop/jarvis-samples-ddd-shop-web/src/main/java/org/yoga/jarvis/config/ResponseAdvice.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright 2022 yoga
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.yoga.jarvis.config;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import org.yoga.jarvis.annotation.OriginalVal;
+import org.yoga.jarvis.bean.Result;
+import org.yoga.jarvis.util.JsonUtils;
+
+/**
+ * @Description: Unified processing of return values
+ * @Author: yoga
+ * @Date: 2022/8/16 18:02
+ */
+@ControllerAdvice
+public class ResponseAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return !returnType.hasMethodAnnotation(OriginalVal.class);
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (selectedConverterType == StringHttpMessageConverter.class) {
+            return JsonUtils.toJsonStr(Result.success(body), "convert fail");
+        }
+        if (body instanceof Result) {
+            return body;
+        }
+        return Result.success(body);
+    }
+}
