2e6152254559a3a0cd9afacd3b4c03e343f4db30
yoga <Gem_yoga@163.com>
2023-03-24 17:56:14 +0800

    添加时间字符串转为LocalDateTime的方法

--- jarvis-core/src/main/java/org/yoga/jarvis/util/DateUtils.java
+++ jarvis-core/src/main/java/org/yoga/jarvis/util/DateUtils.java
@@ -33,105 +33,118 @@ import java.util.Date;
  */
 public class DateUtils {
 
-    /**
-     * default zoneId
-     */
-    private static final ZoneId DEFAULT_ZONE_ID = ZoneId.systemDefault();
+	/**
+	 * default zoneId
+	 */
+	private static final ZoneId DEFAULT_ZONE_ID = ZoneId.systemDefault();
 
-    /**
-     * default zone offset
-     */
-    private static final ZoneOffset DEFAULT_ZONE_OFFSET = DEFAULT_ZONE_ID.getRules().getOffset(Instant.now());
+	/**
+	 * default zone offset
+	 */
+	private static final ZoneOffset DEFAULT_ZONE_OFFSET = DEFAULT_ZONE_ID.getRules().getOffset(Instant.now());
 
-    /**
-     * {@code Date} format as a string with default pattern{@link  DateFormatConstant#FMT_YMD_HMS}
-     *
-     * @param date {@code Date}, not null
-     * @return {@code String}
-     */
-    public static String format(Date date) {
-        return format(date, DateFormatConstant.FMT_YMD_HMS);
-    }
+	/**
+	 * {@code Date} format as a string with default pattern{@link  DateFormatConstant#FMT_YMD_HMS}
+	 *
+	 * @param date {@code Date}, not null
+	 * @return {@code String}
+	 */
+	public static String format(Date date) {
+		return format(date, DateFormatConstant.FMT_YMD_HMS);
+	}
 
-    /**
-     * {@code Date} format as a string with pattern
-     *
-     * @param date    {@code Date}, not null
-     * @param pattern pattern, not blank
-     * @return {@code String}
-     */
-    public static String format(Date date, String pattern) {
-        Assert.notNull(date, "date must not be null!");
-        Assert.notBlank(pattern, "pattern must not be blank!");
-        return formatLocalDateTime(toLocalDateTime(date), pattern);
-    }
+	/**
+	 * {@code Date} format as a string with pattern
+	 *
+	 * @param date    {@code Date}, not null
+	 * @param pattern pattern, not blank
+	 * @return {@code String}
+	 */
+	public static String format(Date date, String pattern) {
+		Assert.notNull(date, "date must not be null!");
+		Assert.notBlank(pattern, "pattern must not be blank!");
+		return formatLocalDateTime(toLocalDateTime(date), pattern);
+	}
 
-    /**
-     * {@code LocalDateTime} format as a string with pattern
-     *
-     * @param localDateTime {@code LocalDateTime}, not null
-     * @param pattern       pattern, not blank
-     * @return {@code String}
-     */
-    public static String formatLocalDateTime(LocalDateTime localDateTime, String pattern) {
-        Assert.notNull(localDateTime, "localDateTime must not be null!");
-        Assert.notBlank(pattern, "pattern must not be blank!");
-        return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
-    }
+	/**
+	 * Convert string to {@code LocalDateTime} with pattern
+	 *
+	 * @param dateStr string(date), not blank
+	 * @param pattern pattern, not blank
+	 * @return {@code LocalDateTime}
+	 */
+	public static LocalDateTime parseLdt(String dateStr, String pattern) {
+		Assert.notBlank(dateStr, "dateStr must not be blank!");
+		Assert.notBlank(pattern, "pattern must not be blank!");
+		return LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern(pattern));
+	}
 
-    /**
-     * Get the {@code LocalDateTime} from a Date
-     *
-     * @param date {@code Date}, not null
-     * @return {@code LocalDateTime}
-     */
-    public static LocalDateTime toLocalDateTime(Date date) {
-        Assert.notNull(date, "date must not be null!");
-        return toZonedDateTime(date).toLocalDateTime();
-    }
+	/**
+	 * {@code LocalDateTime} format as a string with pattern
+	 *
+	 * @param localDateTime {@code LocalDateTime}, not null
+	 * @param pattern       pattern, not blank
+	 * @return {@code String}
+	 */
+	public static String formatLocalDateTime(LocalDateTime localDateTime, String pattern) {
+		Assert.notNull(localDateTime, "localDateTime must not be null!");
+		Assert.notBlank(pattern, "pattern must not be blank!");
+		return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
+	}
 
-    /**
-     * Get the {@code ZonedDateTime} from a Date
-     *
-     * @param date {@code Date}, not null
-     * @return {@code ZonedDateTime} {@link DateUtils#DEFAULT_ZONE_ID}
-     */
-    public static ZonedDateTime toZonedDateTime(Date date) {
-        Assert.notNull(date, "date must not be null!");
-        return date.toInstant().atZone(DEFAULT_ZONE_ID);
-    }
+	/**
+	 * Get the {@code LocalDateTime} from a Date
+	 *
+	 * @param date {@code Date}, not null
+	 * @return {@code LocalDateTime}
+	 */
+	public static LocalDateTime toLocalDateTime(Date date) {
+		Assert.notNull(date, "date must not be null!");
+		return toZonedDateTime(date).toLocalDateTime();
+	}
 
-    /**
-     * get {@code LocalDateTime} from seconds
-     *
-     * @param seconds seconds, Greater than 0
-     * @return {@code LocalDateTime}
-     */
-    public static LocalDateTime fromSeconds(long seconds) {
-        Assert.isTrue(seconds > 0, "seconds must > 0");
-        return LocalDateTime.ofEpochSecond(seconds, 0, DEFAULT_ZONE_OFFSET);
-    }
+	/**
+	 * Get the {@code ZonedDateTime} from a Date
+	 *
+	 * @param date {@code Date}, not null
+	 * @return {@code ZonedDateTime} {@link DateUtils#DEFAULT_ZONE_ID}
+	 */
+	public static ZonedDateTime toZonedDateTime(Date date) {
+		Assert.notNull(date, "date must not be null!");
+		return date.toInstant().atZone(DEFAULT_ZONE_ID);
+	}
 
-    /**
-     * get {@code LocalDateTime} from milliseconds
-     *
-     * @param millSeconds millSeconds, Greater than 0
-     * @return {@code LocalDateTime}
-     */
-    public static LocalDateTime fromMillSeconds(long millSeconds) {
-        Assert.isTrue(millSeconds > 0, "millSeconds must > 0");
-        return LocalDateTime.ofInstant(Instant.ofEpochMilli(millSeconds), DEFAULT_ZONE_ID);
-    }
+	/**
+	 * get {@code LocalDateTime} from seconds
+	 *
+	 * @param seconds seconds, Greater than 0
+	 * @return {@code LocalDateTime}
+	 */
+	public static LocalDateTime fromSeconds(long seconds) {
+		Assert.isTrue(seconds > 0, "seconds must > 0");
+		return LocalDateTime.ofEpochSecond(seconds, 0, DEFAULT_ZONE_OFFSET);
+	}
 
-    /**
-     * get timestamp from {@code LocalDateTime}
-     *
-     * @param localDateTime {@code LocalDateTime}, not null
-     * @return {@code long} timestamp (ms)
-     */
-    public static long toTimestamp(LocalDateTime localDateTime) {
-        Assert.notNull(localDateTime, "localDateTime must not be null!");
-        return localDateTime.toInstant(DEFAULT_ZONE_OFFSET).toEpochMilli();
-    }
+	/**
+	 * get {@code LocalDateTime} from milliseconds
+	 *
+	 * @param millSeconds millSeconds, Greater than 0
+	 * @return {@code LocalDateTime}
+	 */
+	public static LocalDateTime fromMillSeconds(long millSeconds) {
+		Assert.isTrue(millSeconds > 0, "millSeconds must > 0");
+		return LocalDateTime.ofInstant(Instant.ofEpochMilli(millSeconds), DEFAULT_ZONE_ID);
+	}
+
+	/**
+	 * get timestamp from {@code LocalDateTime}
+	 *
+	 * @param localDateTime {@code LocalDateTime}, not null
+	 * @return {@code long} timestamp (ms)
+	 */
+	public static long toTimestamp(LocalDateTime localDateTime) {
+		Assert.notNull(localDateTime, "localDateTime must not be null!");
+		return localDateTime.toInstant(DEFAULT_ZONE_OFFSET).toEpochMilli();
+	}
 
 }
