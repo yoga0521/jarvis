4fd9ca8804f7338a7aacf874bc8b8f47b786104b
yoga <Gem_yoga@163.com>
2023-04-23 16:36:19 +0800

    添加正则工具类

---
+++ jarvis-core/src/main/java/org/yoga/jarvis/util/PatternUtils.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright 2022 yoga
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.yoga.jarvis.util;
+
+import java.util.regex.Pattern;
+
+/**
+ * @Description: Pattern utils
+ * @Author: yoga
+ * @Date: 2023/4/23 16:31
+ */
+public class PatternUtils {
+
+	/**
+	 * Mobile
+	 */
+	public static final Pattern MOBILE = Pattern.compile("(?:0|86|\\+86)?1[3-9]\\d{9}");
+
+	/**
+	 * Hong Kong Mobile
+	 */
+	public static final Pattern MOBILE_HK = Pattern.compile("(?:0|852|\\+852)?\\d{8}");
+
+	/**
+	 * Taiwan Mobile
+	 */
+	public static final Pattern MOBILE_TW = Pattern.compile("(?:0|886|\\+886)?(?:|-)09\\d{8}");
+
+	/**
+	 * Macau Mobile
+	 */
+	public static final Pattern MOBILE_MO = Pattern.compile("(?:0|853|\\+853)?(?:|-)6\\d{7}");
+
+	/**
+	 * Landline number
+	 */
+	public static final Pattern TEL = Pattern.compile("(010|02\\d|0[3-9]\\d{2})-?(\\d{6,8})");
+
+	/**
+	 * email
+	 */
+	public static final Pattern EMAIL = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])", 2);
+
+}
