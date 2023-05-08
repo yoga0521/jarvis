50130b71e848dfdfc2280339d355b0f06be0c062
yoga <Gem_yoga@163.com>
2023-05-08 13:53:16 +0800

    添加香港身份证正则

--- jarvis-core/src/main/java/org/yoga/jarvis/util/PatternUtils.java
+++ jarvis-core/src/main/java/org/yoga/jarvis/util/PatternUtils.java
@@ -46,6 +46,11 @@ public class PatternUtils {
 	public static final Pattern ID_CARD = Pattern.compile("\\d{6}((((((19|20)\\d{2})(0[13-9]|1[012])(0[1-9]|[12]\\d|30))|(((19|20)\\d{2})(0[13578]|1[02])31)|((19|20)\\d{2})02(0[1-9]|1\\d|2[0-8])|((((19|20)([13579][26]|[2468][048]|0[48]))|(2000))0229))\\d{3})|((((\\d{2})(0[13-9]|1[012])(0[1-9]|[12]\\d|30))|((\\d{2})(0[13578]|1[02])31)|((\\d{2})02(0[1-9]|1\\d|2[0-8]))|(([13579][26]|[2468][048]|0[048])0229))\\d{2}))(\\d|X|x)");
 
 	/**
+	 * Hong Kong ID Card
+	 */
+	public static final Pattern ID_CARD_HK = Pattern.compile("[a-zA-Z]\\d{6}\\([\\dA]\\)");
+
+	/**
 	 * Mobile
 	 */
 	public static final Pattern MOBILE = Pattern.compile("(?:0|86|\\+86)?1[3-9]\\d{9}");
