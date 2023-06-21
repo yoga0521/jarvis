9df9b231e407fc6363d1f53afc4c4cc8257935b8
yoga <Gem_yoga@163.com>
2023-06-21 13:14:42 +0800

    添加文件解压和预览接口

---
+++ jarvis-file/src/main/java/org/yoga/jarvis/decompress/DecompressApi.java
@@ -0,0 +1,35 @@
+/*
+ *  Copyright 2022 yoga
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.yoga.jarvis.decompress;
+
+import java.io.File;
+
+/**
+ * @Description: decompress
+ * @Author: yoga
+ * @Date: 2023/6/21 13:10
+ */
+public interface DecompressApi {
+
+    /**
+     * decompress
+     *
+     * @param srcFile compress source file
+     * @param destDir decompressed file dir
+     */
+    void decompress(File srcFile, File destDir);
+}
