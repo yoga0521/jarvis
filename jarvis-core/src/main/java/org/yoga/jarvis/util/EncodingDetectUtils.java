7013a8bf7c8d7093e8ebcaf91cf7903e8862694b
yoga <Gem_yoga@163.com>
2023-08-02 15:27:32 +0800

    添加编码探测工具类

---
+++ jarvis-core/src/main/java/org/yoga/jarvis/util/EncodingDetectUtils.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2022 yoga
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.yoga.jarvis.util;
+
+import java.io.File;
+
+/**
+ * @Description: encoding detect util
+ * @Author: yoga
+ * @Date: 2023/8/2 15:18
+ */
+public class EncodingDetectUtils {
+
+    /**
+     * get file encoding
+     *
+     * @param file file
+     * @return file encoding
+     */
+    public static String getEncode(File file) {
+        Assert.notNull(file, "srcFile is null!");
+        return null;
+    }
+}
