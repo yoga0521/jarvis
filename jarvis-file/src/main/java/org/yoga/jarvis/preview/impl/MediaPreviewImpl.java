7aa78939b6d8835ad7346022cfd4d715b2638450
yoga <Gem_yoga@163.com>
2023-08-04 17:38:42 +0800

    添加media预览实现类

---
+++ jarvis-file/src/main/java/org/yoga/jarvis/preview/impl/MediaPreviewImpl.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 2022 yoga
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.yoga.jarvis.preview.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
+import org.yoga.jarvis.constant.DelimiterType;
+import org.yoga.jarvis.util.FileUtils;
+
+import java.io.File;
+import java.util.UUID;
+
+/**
+ * @Description: media preview
+ * @Author: yoga
+ * @Date: 2023/8/4 17:34
+ */
+@Slf4j
+public class MediaPreviewImpl extends AbstractPreview {
+
+    @Override
+    @NonNull
+    protected File previewActual(@NonNull File srcFile, @NonNull File destDir) {
+        File previewTmpFile = new File(destDir.getPath() + File.separator + UUID.randomUUID() +
+                DelimiterType.point.getValue() + FileUtils.getFileSuffix(srcFile.getName()));
+
+
+        return previewTmpFile;
+    }
+}
