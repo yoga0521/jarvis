5cabcb0f22c1bafc8a142c788f7cdbbc0605c72d
yoga <Gem_yoga@163.com>
2023-09-05 17:19:02 +0800

    实现cmd压缩抽象类

--- jarvis-file/src/main/java/org/yoga/jarvis/compress/AbstractCmdCompress.java
+++ jarvis-file/src/main/java/org/yoga/jarvis/compress/AbstractCmdCompress.java
@@ -16,21 +16,77 @@
 
 package org.yoga.jarvis.compress;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.lang.NonNull;
 import org.yoga.jarvis.CmdHandler;
+import org.yoga.jarvis.exception.JarvisException;
+import org.yoga.jarvis.util.Assert;
+import org.yoga.jarvis.util.StringUtils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.stream.Collectors;
 
 /**
  * @Description: Abstract Cmd Compress
  * @Author: yoga
  * @Date: 2023/8/24 16:32
  */
+@Slf4j
 public abstract class AbstractCmdCompress extends AbstractCompress implements CmdHandler {
 
     @Override
     protected void compressActual(@NonNull File srcFile, @NonNull File destDir) {
+        // check if the os supports the compress command
+        checkCompressCmd();
+        String shell = acquireShell(srcFile.getPath(), destDir.getPath());
+        Assert.notBlank(shell, "compress shell is blank!");
+        Process process = null;
+        InputStream is = null;
+        InputStreamReader isReader = null;
+        BufferedReader br = null;
+        try {
+            process = Runtime.getRuntime().exec(shell);
+            Assert.notNull(process, "compress cmd exec failed!");
+            is = process.getInputStream();
+            isReader = new InputStreamReader(is, Charset.forName("GBK"));
+            br = new BufferedReader(isReader);
+            String compressInfo = br.lines().collect(Collectors.joining("\n"));
+            log.info("compress cmd:\n{}\nexec result:\n{}", shell, compressInfo);
+            if (!isCmdCompressSuccess(compressInfo)) {
+                throw new JarvisException(compressInfo);
+            }
+        } catch (IOException e) {
+            throw new JarvisException(e);
+        } finally {
+            IOUtils.closeQuietly(br);
+            IOUtils.closeQuietly(isReader);
+            IOUtils.closeQuietly(is);
+            if (process != null) {
+                process.destroy();
+            }
+        }
+    }
 
+    /**
+     * check if the os supports the compress command
+     */
+    protected abstract void checkCompressCmd();
+
+    /**
+     * whether compress success
+     * default: if the compress info is empty (no error message), it means success
+     *
+     * @param compressInfo compress info
+     * @return whether compress success
+     */
+    protected boolean isCmdCompressSuccess(String compressInfo) {
+        return StringUtils.isBlank(compressInfo);
     }
 
 }
