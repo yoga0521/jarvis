8ccdbf5e7b0e2b7acbde2277486deb8e20c7e89d
yoga <Gem_yoga@163.com>
2024-06-11 15:23:31 +0800

    添加服务实例对象

---
+++ jarvis-gateway/src/main/java/org/yoga/jarvis/core/ServerInstance.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 yoga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.yoga.jarvis.core;
+
+/**
+ * @Description: Server Instance
+ * @Author: yoga
+ * @Date: 2024/6/11 15:17
+ */
+public class ServerInstance {
+
+    /**
+     * server ip
+     */
+    private String ip;
+
+    /**
+     * server port
+     */
+    private int port;
+
+    /**
+     * server version
+     */
+    private String version;
+
+}
