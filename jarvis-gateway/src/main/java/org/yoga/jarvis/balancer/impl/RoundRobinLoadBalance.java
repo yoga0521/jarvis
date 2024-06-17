02b30ccdea8ba952d9433ac4a54f37d5dbed62ed
yoga <Gem_yoga@163.com>
2024-06-17 15:39:35 +0800

    添加负载均衡轮询算法实现类

---
+++ jarvis-gateway/src/main/java/org/yoga/jarvis/balancer/impl/RoundRobinLoadBalance.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 yoga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.yoga.jarvis.balancer.impl;
+
+import org.yoga.jarvis.balancer.AbstractLoadBalance;
+import org.yoga.jarvis.core.ServerInstance;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Description: Round Robin Load Balance
+ * @Author: yoga
+ * @Date: 2024/6/17 15:27
+ */
+public class RoundRobinLoadBalance extends AbstractLoadBalance {
+
+    public static final String NAME = "round-robin";
+
+    private final AtomicInteger currentIndex = new AtomicInteger(0);
+
+    @Override
+    protected ServerInstance doSelect(List<ServerInstance> instances) {
+        return instances.get(currentIndex.updateAndGet(i -> (i + 1) % instances.size()));
+    }
+}
