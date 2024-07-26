1af8e6beeac40053334686d63c50de37bf40fa9a
yoga <Gem_yoga@163.com>
2024-07-26 15:56:37 +0800

    修改授权插件获取ip的逻辑

--- jarvis-gateway/src/main/java/org/yoga/jarvis/plugin/impl/AuthorizationPlugin.java
+++ jarvis-gateway/src/main/java/org/yoga/jarvis/plugin/impl/AuthorizationPlugin.java
@@ -18,14 +18,21 @@ package org.yoga.jarvis.plugin.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 import org.yoga.jarvis.chain.PluginChain;
 import org.yoga.jarvis.config.ServerConfigs;
 import org.yoga.jarvis.plugin.Plugin;
 import org.yoga.jarvis.util.Assert;
 import org.yoga.jarvis.util.CollectionUtils;
+import org.yoga.jarvis.util.NetUtils;
 import org.yoga.jarvis.util.PatternUtils;
+import org.yoga.jarvis.util.StringUtils;
 import reactor.core.publisher.Mono;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
 /**
  * @Description: Authorization Plugin
@@ -35,6 +42,8 @@ import reactor.core.publisher.Mono;
 public class AuthorizationPlugin implements Plugin {
 
     protected static final Logger logger = LoggerFactory.getLogger(AuthorizationPlugin.class);
+
+    private static final String UNKNOWN = "unknown";
 
     /**
      * Gateway Server Configs
@@ -60,11 +69,55 @@ public class AuthorizationPlugin implements Plugin {
         logger.info("authorization plugin start execute");
         // check ip whitelist
         if (CollectionUtils.isNotEmpty(serverConfigs.getIpWhitelist())) {
-            String ipFormHeaders = exchange.getRequest().getHeaders().getFirst("X-Forwarded-For");
-            Assert.notBlank(ipFormHeaders, "Failed to get the IP address from the request header");
-            String realIp = ipFormHeaders.split(",")[0];
+            String realIp = getRealIpAddress(exchange.getRequest());
             Assert.isTrue(PatternUtils.fuzzyMatch(serverConfigs.getIpWhitelist(), realIp), "IP address is not authorized");
         }
         return pluginChain.execute(exchange, pluginChain);
+    }
+
+    /**
+     * get the real IP address
+     *
+     * @param request http request
+     * @return real IP address
+     */
+    private static String getRealIpAddress(ServerHttpRequest request) {
+        if (request == null) {
+            return null;
+        }
+        HttpHeaders headers = request.getHeaders();
+        String ipAddress = headers.getFirst("X-Forwarded-For");
+        if (StringUtils.isBlank(ipAddress) || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = headers.getFirst("Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(ipAddress) || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = headers.getFirst("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(ipAddress) || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = headers.getFirst("HTTP_CLIENT_IP");
+        }
+        if (StringUtils.isBlank(ipAddress) || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = headers.getFirst("HTTP_X_FORWARDED_FOR");
+        }
+        if (StringUtils.isBlank(ipAddress) || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            InetSocketAddress remoteAddress = request.getRemoteAddress();
+            if (remoteAddress != null) {
+                ipAddress = remoteAddress.getAddress().getHostAddress();
+                if ("127.0.0.1".equals(ipAddress) || "0:0:0:0:0:0:0:1".equals(ipAddress)) {
+                    InetAddress inet = NetUtils.getLocalIpAddress0();
+                    if (null != inet) {
+                        ipAddress = inet.getHostAddress();
+                    }
+                }
+            }
+        }
+
+        // In the case of multiple proxies, the first IP is the real IP of the client
+        // and the multiple IPs are split according to ','
+        // "***.***.***.***".length() = 15
+        if (StringUtils.isNotBlank(ipAddress) && ipAddress.length() > 15) {
+            ipAddress = ipAddress.split(",")[0];
+        }
+        return ipAddress;
     }
 }
