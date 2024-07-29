d8b07e5f3b33ded74fa5c9e5d2cac9cda65b6de4
yoga <Gem_yoga@163.com>
2024-07-29 16:47:42 +0800

    同步已注册的app任务添加获取nacos配置逻辑

--- jarvis-gateway/src/main/java/org/yoga/jarvis/listener/SyncRegisteredAppTaskListener.java
+++ jarvis-gateway/src/main/java/org/yoga/jarvis/listener/SyncRegisteredAppTaskListener.java
@@ -16,16 +16,23 @@
 
 package org.yoga.jarvis.listener;
 
+import com.alibaba.nacos.api.NacosFactory;
 import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.yoga.jarvis.config.ServerConfigs;
+import org.yoga.jarvis.exception.JarvisException;
 import org.yoga.jarvis.factory.ThreadFactoryBuilder;
 import org.yoga.jarvis.task.SyncRegisteredAppTask;
+import org.yoga.jarvis.util.Assert;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -37,6 +44,8 @@ import java.util.concurrent.TimeUnit;
 public class SyncRegisteredAppTaskListener implements ApplicationListener<ContextRefreshedEvent> {
 
     protected static final Logger logger = LoggerFactory.getLogger(SyncRegisteredAppTaskListener.class);
+
+    private static final String NACOS_DATA_ID = "app-config";
 
     private static final ScheduledThreadPoolExecutor scheduledPool = new ScheduledThreadPoolExecutor(1,
             new ThreadFactoryBuilder().setNameFormat("sync-registered-app").build());
@@ -53,5 +62,37 @@ public class SyncRegisteredAppTaskListener implements ApplicationListener<Contex
         logger.info("server refresh interval: {}s", serverConfigs.getCacheRefreshInterval());
         scheduledPool.scheduleWithFixedDelay(new SyncRegisteredAppTask(namingService), 0L,
                 serverConfigs.getCacheRefreshInterval(), TimeUnit.MICROSECONDS);
+        // config
+        try {
+            String serverAddr = event.getApplicationContext().getEnvironment().getProperty("nacos.discovery.server-addr");
+            Assert.notBlank(serverAddr, "nacos discovery server-addr must not be null");
+            ConfigService configService = NacosFactory.createConfigService(serverAddr);
+            // get config from nacos
+            String configInfo = configService.getConfig(NACOS_DATA_ID, SyncRegisteredAppTask.APP_GROUP_NAME, 5000);
+            // init config
+            updateConfig(configInfo);
+            // add nacos config listener
+            configService.addListener(NACOS_DATA_ID, SyncRegisteredAppTask.APP_GROUP_NAME, new Listener() {
+
+                @Override
+                public Executor getExecutor() {
+                    return null;
+                }
+
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    logger.info("receive configs from nacos: {}", configInfo);
+                    // update config
+                    updateConfig(configInfo);
+                }
+            });
+        } catch (NacosException e) {
+            throw new JarvisException("get configs from nacos error");
+        }
+    }
+
+
+    private void updateConfig(String configInfo) {
+        logger.info("update config success");
     }
 }
