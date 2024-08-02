3e15c787ac744f7c85269a17fe74380a9782cb0d
yoga <Gem_yoga@163.com>
2024-08-02 15:03:13 +0800

    json工具类添加配置信息和json转对象的方法

--- jarvis-core/src/main/java/org/yoga/jarvis/util/JsonUtils.java
+++ jarvis-core/src/main/java/org/yoga/jarvis/util/JsonUtils.java
@@ -16,10 +16,19 @@
 
 package org.yoga.jarvis.util;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.yoga.jarvis.constant.DateFormatConstant;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 /**
  * @Description: Json Utils
@@ -32,6 +41,19 @@ public class JsonUtils {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
+    static {
+        // serialize all fields
+        objectMapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+        // cancel the conversion to timestamps
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        // ignore the error of converting empty bean to json
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        // set the date format
+        objectMapper.setDateFormat(new SimpleDateFormat(DateFormatConstant.FMT_YMD_HMS));
+        // ignore properties that don't exist in java objects
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
     /**
      * json string parse object
      *
@@ -40,7 +62,7 @@ public class JsonUtils {
      * @param <T>     generics
      * @return object
      */
-    public static <T> T parseObject(String jsonStr, Class<T> clazz) {
+    public static <T> T parseObj(String jsonStr, Class<T> clazz) {
         try {
             return objectMapper.readValue(jsonStr, clazz);
         } catch (JsonProcessingException e) {
@@ -50,12 +72,49 @@ public class JsonUtils {
     }
 
     /**
+     * json string parse object
+     *
+     * @param jsonStr   json string
+     * @param reference type reference
+     * @param <T>       generics
+     * @return object
+     */
+    public static <T> T parseObj(String jsonStr, TypeReference<T> reference) {
+        try {
+            return objectMapper.readValue(jsonStr, reference);
+        } catch (JsonProcessingException e) {
+            logger.error("json string parse object fail", e);
+        }
+        return null;
+    }
+
+    /**
+     * json string (from file) parse object
+     *
+     * @param file  file
+     * @param clazz class of object
+     * @param <T>   generics
+     * @return object
+     */
+    public static <T> T parseObj(File file, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(file, clazz);
+        } catch (IOException e) {
+            logger.error("failed to read JSON string from file to object", e);
+        }
+        return null;
+    }
+
+    //将json数组字符串转为指定对象List列表或者Map集合
+
+
+    /**
      * object to json string
      *
      * @param obj object
      * @return json string
      */
-    public static String toJSONString(Object obj) {
+    public static String toJSONStr(Object obj) {
         try {
             return objectMapper.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
@@ -63,4 +122,7 @@ public class JsonUtils {
         }
         return null;
     }
+
+
+
 }
