401ae73e6466a12f29179083f3f3530ace69f5a7
yoga <Gem_yoga@163.com>
2024-08-13 13:42:13 +0800

    添加jarvis-admin模块

---
+++ jarvis-admin/src/main/java/org/yoga/jarvis/JarvisAdminApplication.java
@@ -0,0 +1,13 @@
+package org.yoga.jarvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarvisAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarvisAdminApplication.class, args);
+    }
+
+}
---
+++ jarvis-admin/src/test/java/org/yoga/jarvis/JarvisAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yoga.jarvis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarvisAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
