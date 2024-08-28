caa832abf90ea405d4d64420b1e39615d43a3a1f
yoga <Gem_yoga@163.com>
2024-08-28 13:31:32 +0800

    添加mvc配置

---
+++ jarvis-admin/src/main/java/org/yoga/jarvis/bean/config/MvcConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 yoga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.yoga.jarvis.bean.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description: mvc配置
+ * @Author: yoga
+ * @Date: 2024/8/28 13:30
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(true)
+                //放行哪些原始域
+                .allowedOriginPatterns("*")
+                // 后续有其他请求方式再放开
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name())
+                .allowedHeaders("*");
+    }
+}
